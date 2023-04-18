bde8baa8a6d93bea1e74d581a63cd4b562483750
KIM WONHEE <49189226+wooneeS2@users.noreply.github.com>
2023-04-18 14:53:39 +0900

    프로젝트 이동

---
+++ board/.next/server/flight-server-css-manifest.js
@@ -0,0 +1,8 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {},
+  "cssModules": {
+    "/Users/gim-wonhui/Desktop/project/next_board/board/src/app/page": [],
+    "/Users/gim-wonhui/Desktop/project/next_board/board/src/app/article/[articleId]/page": [],
+    "/Users/gim-wonhui/Desktop/project/next_board/board/src/app/writing/page": []
+  }
+}
